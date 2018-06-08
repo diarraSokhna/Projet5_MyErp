5eccf9ee57968a16a94af01e428277221017922b
sokhna <khadisankhe@gmail.com>
2018-06-08 10:45:17 +0200

    Premier commit

---
+++ myerp/myerp-consumer/src/main/java/com/dummy/myerp/consumer/dao/contrat/DaoProxy.java
@@ -0,0 +1,17 @@
+package com.dummy.myerp.consumer.dao.contrat;
+
+
+/**
+ * Interface du Proxy d'accès à la couche DAO
+ */
+public interface DaoProxy {
+
+    /**
+     * Renvoie un {@link ComptabiliteDao}
+     *
+     * @return {@link ComptabiliteDao}
+     */
+    ComptabiliteDao getComptabiliteDao();
+ 
+
+}
