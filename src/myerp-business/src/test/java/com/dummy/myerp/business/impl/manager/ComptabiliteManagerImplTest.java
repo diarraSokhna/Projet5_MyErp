4ed36964ba3ed0fffaa17adf67bb14e06126255b
BAR Khady <khadisankhe@gmail.com>
2018-07-10 07:37:37 +0200

    Update ComptabiliteManagerImplTest.java

--- src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
+++ src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
@@ -49,7 +49,7 @@ public class ComptabiliteManagerImplTest extends BusinessTestCase {
 
    @Test
     public void getEcritureComptable() throws NotFoundException {
-        EcritureComptable vEcritureComptable = managerIntegration.getEcritureComptable(-3);
+        EcritureComptable vEcritureComptable = manager.getEcritureComptable(-3);
         Assert.assertEquals("BQ-2016/00003", vEcritureComptable.getReference());
 
     }
