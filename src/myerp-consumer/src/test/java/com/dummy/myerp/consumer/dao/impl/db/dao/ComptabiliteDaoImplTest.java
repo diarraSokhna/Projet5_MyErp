19f7209bdde8ce0dd983d9b01ae41ca83ba7a60f
BAR Khady <khadisankhe@gmail.com>
2018-07-13 16:10:51 +0200

    Update ComptabiliteDaoImplTest.java

--- src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/dao/ComptabiliteDaoImplTest.java
+++ src/myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/dao/ComptabiliteDaoImplTest.java
@@ -47,7 +47,7 @@ public class ComptabiliteDaoImplTest  extends ConsumerTestCase{
    @Test
     public void getListEcritureComptable() {
         List<EcritureComptable> vList = dao.getListEcritureComptable();
-        assertEquals(12, vList.size());
+        assertEquals(5, vList.size());
     }
 
     @Test
