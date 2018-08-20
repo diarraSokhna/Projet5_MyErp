74c7fe664f5fb34d3eef1192771ecf784838249d
BAR Khady <khadisankhe@gmail.com>
2018-08-20 21:59:13 +0200

    Update EcritureComptable.java

--- src/myerp-model/src/main/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptable.java
+++ src/myerp-model/src/main/java/com/dummy/myerp/model/bean/comptabilite/EcritureComptable.java
@@ -23,7 +23,7 @@ public class EcritureComptable {
 	@NotNull
 	private JournalComptable journal;
 	/** The Reference. */
-	@Pattern(regexp = "\\d{1,5}-\\d{4}/\\d{5}")
+	@Pattern(regexp = "[A-Z]{1,5}-\\d{4}/\\d{5}")
 	private String reference;
 	/** The Date. */
 	@NotNull
